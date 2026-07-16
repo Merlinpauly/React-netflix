d52adfd86931622a3d225ee377d1406eb8fb3acd
Merlin Pauly <merlinpauly85@gmail.com>
2026-07-16 12:22:01 +0530

    Initial React Netflix project

---
+++ React Netflix/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+function App(){
+  const[username , setUserName] = useState("");
+  const[email , setEmail] = useState("");
+  function handlenameChange(){
+    setUserName(event.target.value);
+  }
+  function handleemailChange(){
+    setEmail(event.target.value);
+  }
+  return (
+    <div className="Login-form">
+      <form>
+        <label>Username</label>
+        <input type="text" value={username} onChange={handlenameChange} placeholder="Enter your Username"/>
+        <label>Email ID</label>
+        <input type="email" value={email} onChange={handleemailChange} placeholder="Enter your Email id"/>
+
+
+      </form>
+    </div>
+    
+  );
+}
+
+export default App;
